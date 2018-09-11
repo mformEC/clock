934f2eb47418837dd7fb328fd20aa8023030cb00
george <wubf.george@foxmail.com>
2018-09-11 09:48:44 +0800

    update
    while hour < 10 , will caused error. all right now

--- function.bat
+++ function.bat
@@ -18,10 +18,13 @@ if %var:~3,1% equ 0 (set /a min=%var:~4,1% + %waitTime%) else (set /a min=%var:~
 if %min% geq 60 (set /a min=%min% - 60 & set /a hour=%hour% + 1)
 if %hour% geq 24 (set /a hour=%hour% - 24)
 if %min% lss 10 (set min=0%min%)
+if %hour% lss 10 (set hour=0%TIME:~1,1%)
+
 
 set command1='schtasks /Create /SC ONCE /TN %taskName% /TR %strToShow% /ST %hour%:%min% /F
 set command1=%command1%'
-rem schtasks /Create /SC ONCE /TN %taskName% /TR %strToShow% /ST %hour%:%min% /F
+
+echo %command1%
 
 for /f "delims=  tokens=1,*" %%i in (%command1%) do (set result1=%%i %%j)
 if "%result1%"=="" (set result1=check on console can't get any msg back??) 
